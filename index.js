3481c45a68d255f207167fccbd6dd447e68629f4
Bob Oyier <boboyier92@gmail.com>
2022-11-18 16:26:42 +0300

    "Corrected a typo."

--- index.js
+++ index.js
@@ -3,34 +3,27 @@ function shout(string) {
 }
 shout("string");
 
-
 function whisper(string) {
   return string.toLowerCase();
 }
 whisper("string");
 
-
 function logShout(string) {
   console.log(string.toUpperCase());
 }
 
-
 function logWhisper(string) {
-	console.log(string.toLowerCase())
+  console.log(string.toLowerCase());
 }
 
 function sayHiToHeadphonedRoommate(string) {
-	if (string === string.toLowerCase()) {
-		return "I can't hear you!"
-	}
-
-	else if (string === string.toUpperCase()) {
-		return "YES INDEED!"
-	}
-
-	else if (string === "Let's have dinner together!") {
-		return "I would love to!"
-	}
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+  } else if (string === "Let's have dinner together!") {
+    return "I would love to!";
+  }
 }
 
-sayHiToHeadphonedRoommate("string")
+sayHiToHeadphonedRoommate("string");
